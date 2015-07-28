abd8880e252c4f57e000b7b7c97c53de871e2476
Markus Padourek <markus.padourek@gmail.com>
2015-07-28 01:37:24 +0100

    Initial release of broccoli-browser-sync.

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # broccoli-browser-sync
 BrowserSync support for Broccolijs
+
+## Usage
+
+Right now usage is still a bit suboptimal, but you already get all the beauty of BrowserSync including live css injection as well as livereload.
+
+```js
+var browserSync = new BrowserSync(inputTrees);
+```
+
+e.g.
+```js
+var fastBrowserify = require('broccoli-fast-browserify');
+var babelify = require('babelify');
+var mergeTrees = require('broccoli-merge-trees');
+var compileSass = require('broccoli-sass-source-maps');
+var funnel = require('broccoli-funnel');
+var BrowserSync = require('broccoli-browser-sync');
+
+var optionalTransforms = [
+  'regenerator'
+  // 'minification.deadCodeElimination',
+  // 'minification.inlineExpressions'
+];
+
+var babelOptions = {stage: 0, optional: optionalTransforms, compact: true};
+
+// var browserifyOpts = {deps: true, entries: files, noParse: noParse, ignoreMissing: true};
+var transformedBabelify = fastBrowserify('app', {
+  browserify: {
+    extensions: [".js"]
+  },
+  bundles: {
+    'js/app.js': {
+      entryPoints: ['app.js'],
+      transform: {
+        tr: babelify,
+        options: {
+          stage: 0
+        }
+      }
+    }
+  }
+});
+
+var appCss = compileSass(['piggy/frontend/app'], 'main.scss', 'css/app.css');
+
+var staticFiles = funnel('frontend', {
+  srcDir: 'static'
+});
+
+var browserSync = new BrowserSync([staticFiles, transformedBabelify, appCss]);
+
+module.exports = mergeTrees([staticFiles, transformedBabelify, appCss, browserSync]);
+```
---
+++ index.js
@@ -0,0 +1,65 @@
+'use strict';
+var glob = require('glob');
+var bs = require('browser-sync');
+var path = require('path');
+var CachingWriter = require('broccoli-caching-writer');
+
+function BrowserSyncWatcher(inputTrees, options) {
+  if (!(this instanceof BrowserSyncWatcher)) {
+    return new BrowserSyncWatcher(inputTrees, options);
+  }
+
+  // the options should override that in the CachingWrite if passed
+  this.onlyReturnUpdatedTrees = true;
+
+  CachingWriter.call(this, inputTrees, options);
+
+  options = options || {};
+  this.options = options;
+  this.port = (options.port > 0 ? options.port : 4200 );
+  this.bsInstance = bs.create();
+
+  this.bsInstance.init({
+    proxy: 'http://localhost:' + this.port
+  });
+
+  this.reload = function (files) {
+    this.bsInstance.reload(files);
+  };
+}
+
+BrowserSyncWatcher.prototype = Object.create(CachingWriter.prototype);
+BrowserSyncWatcher.prototype.constructor = BrowserSyncWatcher;
+
+// browser-sync can only inject css files at the moment
+BrowserSyncWatcher.prototype.extensions = ['css'];
+
+// TODO(markus): write custom caching functionality that either returns
+// all the files that have been updated, or better: It returns all updated
+// files with given extensions if those are the only files that have been updated.
+// otherwise it will return an empty array.
+BrowserSyncWatcher.prototype.updateCache = function(srcPaths, destDir) {
+  var files = [];
+  for (var i = 0; i < srcPaths.length; i++) {
+    var tmpFiles = glob.sync(path.join(srcPaths[i], '/**/*'), { nodir: true });
+    for (var j = 0; j < tmpFiles.length; j++) {
+      var fileEnding = '.' + this.extensions[0];
+      // if a filename does not end with the specified fileEnding then reload
+      // the whole page and exit.
+      if (tmpFiles[j].indexOf(fileEnding, this.length - fileEnding.length) === -1) {
+        this.reload();
+        return;
+      }
+    }
+    files = files.concat(tmpFiles);
+  }
+
+  // if we have any css file then inject these.
+  if (files.length > 0) {
+    console.log('injecting css', files);
+    this.reload(files);
+  }
+  return;
+};
+
+module.exports = BrowserSyncWatcher;
